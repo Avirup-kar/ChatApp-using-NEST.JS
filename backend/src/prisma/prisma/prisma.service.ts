83b80fe0c1ec79ff75cce78156a31b0f353925f5
Avirup-kar <avirupk57@gmail.com>
2026-06-04 21:15:43 +0530

    add again

--- backend/src/prisma/prisma/prisma.service.ts
+++ backend/src/prisma/prisma/prisma.service.ts
@@ -4,6 +4,7 @@ import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 // import { PrismaClient } from '../../generated/prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from 'generated/prisma/client';
+import { Pool } from 'pg';
 
 @Injectable()
 export class PrismaService
@@ -11,7 +12,11 @@ export class PrismaService
   implements OnModuleInit, OnModuleDestroy
 {
   constructor() {
-    const adapter = new PrismaPg(process.env.DATABASE_URL!);
+    const pool = new Pool({
+      connectionString: process.env.DATABASE_URL!,
+    });
+
+    const adapter = new PrismaPg(pool);
     super({ adapter });
   }
 
