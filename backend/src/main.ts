e3824596bca91a3b538ff05362e8c9441d564d33
Avirup-kar <avirupk57@gmail.com>
2026-06-05 17:34:05 +0530

    add again

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
 import graphqlUploadExpress from 'graphql-upload/graphqlUploadExpress.mjs';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -27,6 +28,24 @@ async function bootstrap() {
     graphqlUploadExpress({
       maxFileSize: 10_000_000, // 10 MB
       maxFiles: 1,
+    }),
+  );
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      exceptionFactory: (errors) => {
+        const formattedErrors = errors.reduce((accumulator, error) => {
+          if (error.constraints) {
+            accumulator[error.property] = Object.values(error.constraints).join(
+              ', ',
+            );
+          }
+          return accumulator;
+        }, {});
+        throw new BadRequestException(formattedErrors);
+      },
     }),
   );
 
