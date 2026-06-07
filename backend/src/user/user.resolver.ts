3f766d11b56b479586ff54cdb571abbc699dc0ee
Avirup-kar <avirupk57@gmail.com>
2026-06-08 00:06:30 +0530

    add again

--- backend/src/user/user.resolver.ts
+++ backend/src/user/user.resolver.ts
@@ -1,9 +1,19 @@
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Query, Context, Mutation, Args } from '@nestjs/graphql';
 import { UserService } from './user.service';
+import { User } from './user.type';
+import { Request } from 'express';
+import { UseGuards } from '@nestjs/common';
+import { GraphqlAuthGuard } from 'src/auth/graphql_auth.guard';
+import { createWriteStream } from 'fs';
+import { join } from 'path';
+import { v4 as uuidv4 } from 'uuid';
+import * as GraphQLUpload from 'graphql-upload/GraphQLUpload.mjs';
 
 @Resolver()
 export class UserResolver {
   constructor(private readonly userService: UserService) {}
 
-  
+  @Mutation(() => User)
+
+
 }
