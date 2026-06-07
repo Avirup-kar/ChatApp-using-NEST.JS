b058559a21f491ed724ce160b715704c74fed5fe
Avirup-kar <avirupk57@gmail.com>
2026-06-07 21:12:34 +0530

    craete auth resolver

--- backend/src/auth/auth.resolver.ts
+++ backend/src/auth/auth.resolver.ts
@@ -1,8 +1,27 @@
-import { Resolver, Query } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args, Context } from '@nestjs/graphql';
 import { AuthService } from './auth.service';
+import { RegisterResponse } from './types';
+import { RegisterDto } from './dto';
+import { Response } from 'express';
+import { BadRequestException } from '@nestjs/common';
 @Resolver()
 export class AuthResolver {
   constructor(private readonly authService: AuthService) {}
+
+  @Mutation(() => RegisterResponse)
+  register(
+    @Args('registerInput') registerDto: RegisterDto,
+    @Context() context: { res: Response },
+  ) {
+    if (registerDto.password != registerDto.confirmPassword) {
+      throw new BadRequestException({
+        confirmPassword: 'Password and confirm password are not the same.',
+      });
+    }
+    const user = this.authService.register(registerDto, context.res);
+    return user;
+  }
+
   @Query(() => String)
   hello() {
     return 'hello';
