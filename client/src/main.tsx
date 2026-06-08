0a41e52e6d90943a296c576a72ce4e309089f550
Avirup-kar <avirupk57@gmail.com>
2026-06-08 20:59:01 +0530

    add again

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
@@ -7,8 +6,6 @@ import { MantineProvider } from '@mantine/core'
 
 createRoot(document.getElementById('root')!).render(
   <MantineProvider>
-  <StrictMode>
     <App />
-  </StrictMode>
   </MantineProvider>
 )
