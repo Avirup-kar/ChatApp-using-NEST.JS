0d76ae6babdf232d48c571956a6646dde874e44d
Avirup-kar <avirupk57@gmail.com>
2026-06-08 21:30:44 +0530

    add again

--- client/codegen.ts
+++ client/codegen.ts
@@ -1,8 +1,8 @@
 import type { CodegenConfig } from '@graphql-codegen/cli'
  
 const config: CodegenConfig = {
-  schema: 'https://graphql.org/graphql/',
-  documents: ['src/**/*.tsx'],
+  schema: 'http://localhost:3000/graphql',
+  documents: ['src/graphql/**/*.ts'],
   ignoreNoDocuments: true, // for better experience with the watcher
   generates: {
     './src/gql/': {
