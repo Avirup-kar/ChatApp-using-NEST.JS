bf4d29038671f0e4c06447240531f3d425db0a63
Avirup-kar <avirupk57@gmail.com>
2026-06-09 21:46:21 +0530

    add sgain

--- client/src/apolloClient.ts
+++ client/src/apolloClient.ts
@@ -1,15 +1,41 @@
 import {
   ApolloClient,
   InMemoryCache,
-  NormalizedCacheObject,
   gql,
   Observable,
   ApolloLink,
   split,
+  type TypedDocumentNode,
 } from "@apollo/client";
-import { WebSocketLink } from "@apollo/client/link/ws"
-import { createUploadLink } from "apollo-upload-client"
-import { getMainDefinition } from "@apollo/client/utilities"
-import { loadErrorMessages, loadDevMessages } from "@apollo/client/dev"
-import { useUserStore } from "./stores/userStore"
-import { onError } from "@apollo/client/link/error"
+import { WebSocketLink } from "@apollo/client/link/ws";
+import { createUploadLink } from "apollo-upload-client";
+import { getMainDefinition } from "@apollo/client/utilities";
+import { loadErrorMessages, loadDevMessages } from "@apollo/client/dev";
+import { useUserStore } from "./stores/userStore";
+import { onError } from "@apollo/client/link/error";
+
+loadErrorMessages();
+loadDevMessages();
+
+async function refreshToken(client: ApolloClient) {
+  try {
+    const REFRESH_TOKEN_MUTATION: TypedDocumentNode<{ refreshToken: string }> = gql`
+      mutation RefreshToken {
+        refreshToken
+      }
+    `;
+
+    const { data } = await client.mutate({
+      mutation: REFRESH_TOKEN_MUTATION,
+    });
+
+    const newAccessToken = data?.refreshToken;
+    if (!newAccessToken) {
+      throw new Error("New access token not received.");
+    }
+    return `Bearer ${newAccessToken}`;
+  } catch (err) {
+    throw new Error("Error getting new access token.", { cause: err });
+  }
+}
+
