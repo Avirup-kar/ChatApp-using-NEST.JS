8420191006f075f8467d3ef9cb6a2281c7015c4c
Avirup-kar <avirupk57@gmail.com>
2026-06-11 16:52:51 +0530

    add sgain

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,9 +3,13 @@ import './index.css'
 import App from './App.tsx'
 import "@mantine/core/styles.css";
 import { MantineProvider } from '@mantine/core'
+import { ApolloProvider } from '@apollo/client/react';
+import { client } from './apolloClient.ts';
 
 createRoot(document.getElementById('root')!).render(
   <MantineProvider>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </MantineProvider>
 )
