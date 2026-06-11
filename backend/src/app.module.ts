9e7f4eed083613889057f2982d8d072f1f7c7df0
Avirup-kar <avirupk57@gmail.com>
2026-06-11 18:25:19 +0530

    add sgain

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -8,6 +8,18 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { RedisPubSub } from 'graphql-redis-subscriptions';
+
+const pubSub = new RedisPubSub({
+  connection: {
+    host: process.env.REDIS_HOST || 'localhost',
+    port: parseInt(process.env.REDIS_PORT || '6379', 10),
+    retryStrategy: (times) => {
+      // retry strategy
+      return Math.min(times * 50, 2000);
+    },
+  },
+});
 
 @Module({
   imports: [
