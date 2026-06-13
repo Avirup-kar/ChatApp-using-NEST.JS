08033917ef0709ceaab9d9af2f3ab924c64332a5
Avirup-kar <avirupk57@gmail.com>
2026-06-13 19:47:53 +0530

    made a protected routes

--- client/src/component/ProtectedRoutes.tsx
+++ client/src/component/ProtectedRoutes.tsx
@@ -6,5 +6,16 @@ const ProtectedRoutes: React.FC<{ children: React.ReactNode }> = ({ children }) 
     const userId = useUserStore((state) => state.id);
     const toggleLoginModal = useGeneralStore((state) => state.toggleLoginModal);
 
-    
+   useEffect(() => {
+    if (!userId) {
+      toggleLoginModal()
+    }
+  }, [toggleLoginModal, userId])
+
+   if (userId) {
+    return children
+  }
+  return <>Protected</>
 }
+
+export default ProtectedRoutes
