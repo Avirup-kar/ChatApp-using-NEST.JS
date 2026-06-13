c9aa9570708952a9c1ff3c9e63dc8cf017f6412d
Avirup-kar <avirupk57@gmail.com>
2026-06-13 20:37:52 +0530

    add again

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,15 +1,28 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from 'react-dom/client';
+import './index.css';
 import "@mantine/core/styles.css";
 import { MantineProvider } from '@mantine/core'
 import { ApolloProvider } from '@apollo/client/react';
 import { client } from './apolloClient.ts';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './pages/Home.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    children: [
+      {
+        path: "/chatrooms/:id",
+      },
+    ],
+  },
+])
 
 createRoot(document.getElementById('root')!).render(
     <ApolloProvider client={client}>
       <MantineProvider>
-          <App />
+          <RouterProvider router={router} />
       </MantineProvider>
     </ApolloProvider>
 )
