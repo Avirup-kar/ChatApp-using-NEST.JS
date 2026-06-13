4fafb6414b6284cdd52341f24bf36d848405c42b
Avirup-kar <avirupk57@gmail.com>
2026-06-13 20:25:36 +0530

    add again

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import MainLayout from '../layouts/MainLayout'
 import Sidebar from '../component/Sidebar'
+import ProtectedRoutes from '../component/ProtectedRoutes'
 
 const Home = () => {
   return (
     <MainLayout>
-     <>
+     <ProtectedRoutes>
       <Sidebar/>
       Home
-     </>
+     </ProtectedRoutes>
     </MainLayout>
   )
 }
